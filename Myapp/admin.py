944967be53ec429e273346b5082bbd21b2a42934
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-12-06 18:00:47 +0530

    checkout db registered in myapp admin

--- Myapp/admin.py
+++ Myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from Myapp.models import bookdb,catergorydb
-from webapp.models import contactdb,cartdb
+from webapp.models import contactdb,cartdb,Checkoutdb
 # Register your models here.
 admin.site.register(catergorydb)
 admin.site.register(bookdb)
 admin.site.register(contactdb)
 admin.site.register(cartdb)
+admin.site.register(Checkoutdb)
