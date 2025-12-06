acec0a9496ec462493d5b8627e8d55b1ffa8f768
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-12-06 10:28:38 +0530

    receipt in cart  set

--- webapp/views.py
+++ webapp/views.py
@@ -14,7 +14,7 @@ def Homepage(request):
     if uname:
         cart_count = cartdb.objects.filter(Singlebook_username=uname).count()
 
-    return render(request, "Homepage.html", {'category': category, 'books': books,'cart_count':cart_count})
+    return render(request, "Homepage.html", {'category': category, 'books': books, 'cart_count': cart_count})
 
 
 def Aboutpage(request):
@@ -25,7 +25,7 @@ def Aboutpage(request):
     if uname:
         cart_count = cartdb.objects.filter(Singlebook_username=uname).count()
 
-    return render(request, "aboutpage.html", {'category': category,'cart_count':cart_count})
+    return render(request, "aboutpage.html", {'category': category, 'cart_count': cart_count})
 
 
 def Contactpage(request):
@@ -35,7 +35,7 @@ def Contactpage(request):
         cart_count = cartdb.objects.filter(Singlebook_username=uname).count()
 
     category = catergorydb.objects.all()
-    return render(request, "contact.html", {'category': category,'cart_count':cart_count})
+    return render(request, "contact.html", {'category': category, 'cart_count': cart_count})
 
 
 def Popularpage(request):
@@ -46,7 +46,7 @@ def Popularpage(request):
 
     books = bookdb.objects.all()
     category = catergorydb.objects.all()
-    return render(request, "popular.html", {'category': category, 'books': books,'cart_count':cart_count})
+    return render(request, "popular.html", {'category': category, 'books': books, 'cart_count': cart_count})
 
 
 def Checkoutpage(request):
@@ -56,7 +56,8 @@ def Checkoutpage(request):
         cart_count = cartdb.objects.filter(Singlebook_username=uname).count()
 
     category = catergorydb.objects.all()
-    return render(request, "checkoutpage.html", {'category': category,'cart_count':cart_count})
+    return render(request, "checkoutpage.html", {'category': category, 'cart_count': cart_count})
+
 
 def Filterbooks(request, category_name):
     cart_count = 0
@@ -66,7 +67,8 @@ def Filterbooks(request, category_name):
 
     category = catergorydb.objects.all()
     books = bookdb.objects.filter(Book_category=category_name)
-    return render(request, "Filter_books.html", {'category': category, 'books': books, 'category_name': category_name,'cart_count':cart_count})
+    return render(request, "Filter_books.html",
+                  {'category': category, 'books': books, 'category_name': category_name, 'cart_count': cart_count})
 
 
 def Singlebook(request, book_id):
@@ -77,7 +79,7 @@ def Singlebook(request, book_id):
 
     category = catergorydb.objects.all()
     books = bookdb.objects.get(id=book_id)
-    return render(request, "singlebook.html", {'books': books, 'category': category,'cart_count':cart_count})
+    return render(request, "singlebook.html", {'books': books, 'category': category, 'cart_count': cart_count})
 
 
 def savecontact(request):
@@ -92,49 +94,74 @@ def savecontact(request):
                        Contact_message=contact_message)
 
         ob.save()
-        messages.success(request,"Feedback Added Succesfully!")
+        messages.success(request, "Feedback Added Succesfully!")
         return redirect(Contactpage)
 
 
 def Cartpage(request):
     cart = cartdb.objects.filter(Singlebook_username=request.session['username'])
-    category=catergorydb.objects.all()
-    books=bookdb.objects.all()
+    category = catergorydb.objects.all()
+    books = bookdb.objects.all()
 
-    cart_count=0
+    cart_count = 0
     uname = request.session.get('username')
     if uname:
-        cart_count=cartdb.objects.filter(Singlebook_username=uname).count()
-    return render(request, "Cartpage.html", {'cart': cart,'category':category,'books':books,'cart_count':cart_count})
+        cart_count = cartdb.objects.filter(Singlebook_username=uname).count()
+
+    # receipt calculation
+    sub_total = 0
+    total_amount = 0
+    delivery = 0
+    gst = 0
+    for i in cart:
+        sub_total += i.Singlebook_total
+        if sub_total < 500:
+            delivery = 50
+        else:
+            delivery = 0
+        gst = round((sub_total+delivery) * 0.05)
+        total_amount = round(sub_total + gst + delivery)
+
+
+
+    return render(request, "Cartpage.html",
+                  {'cart': cart,
+                   'category': category,
+                   'books': books,
+                   'cart_count': cart_count,
+                   'sub_total': sub_total,
+                   'delivery': delivery,
+                   'gst': gst,
+                   'total_amount': total_amount})
+
 
 def savecart(request):
-    if request.method=='POST':
-        singlebook_quantity=request.POST.get('singlebook_quantity')
-        singlebook_total=request.POST.get('singlebook_total')
-        singlebook_title=request.POST.get('singlebook_title')
-        singlebook_price=request.POST.get('singlebook_price')
-        singlebook_username=request.POST.get('singlebook_username')
-        book=bookdb.objects.filter(Book_title=singlebook_title).first()
+    if request.method == 'POST':
+        singlebook_quantity = request.POST.get('singlebook_quantity')
+        singlebook_total = request.POST.get('singlebook_total')
+        singlebook_title = request.POST.get('singlebook_title')
+        singlebook_price = request.POST.get('singlebook_price')
+        singlebook_username = request.POST.get('singlebook_username')
+        book = bookdb.objects.filter(Book_title=singlebook_title).first()
         singlebook_image = book.Book_cover
-        bookid=book.id
+        bookid = book.id
 
-
-        ob=cartdb(Singlebook_username=singlebook_username,
+        ob = cartdb(Singlebook_username=singlebook_username,
                     Singlebook_title=singlebook_title,
                     Singlebook_price=singlebook_price,
                     Singlebook_quantity=singlebook_quantity,
                     Singlebook_total=singlebook_total,
                     Singlebook_image=singlebook_image,
-                  Bookid=bookid)
+                    Bookid=bookid)
 
         ob.save()
-        messages.success(request,"Added to Cart !")
+        messages.success(request, "Added to Cart !")
         return redirect(Homepage)
 
-def deletecart(request,c_id):
-    data=cartdb.objects.filter(id=c_id).delete()
-    return redirect(Cartpage)
 
+def deletecart(request, c_id):
+    data = cartdb.objects.filter(id=c_id).delete()
+    return redirect(Cartpage)
 
 
 def Usersigninpage(request):
@@ -144,17 +171,16 @@ def Usersigninpage(request):
 def Usersignuppage(request):
     return render(request, "Usersignuppage.html")
 
+
 def SignupDisplaypage(request):
-    data=signupdb.objects.all()
-    return render(request,"signupdbdisplay.html",{'data':data})
+    data = signupdb.objects.all()
+    return render(request, "signupdbdisplay.html", {'data': data})
+
 
 def DeleteSignup(request, user_id):
     user = signupdb.objects.get(id=user_id)
     user.delete()
     return redirect("SignupDisplaypage")
-
-
-
 
 
 def Saveusersignup(request):
@@ -172,17 +198,17 @@ def Saveusersignup(request):
                       Signup_confirm=signup_confirm)
 
         if signupdb.objects.filter(Signup_username=signup_username).exists():
-            messages.error(request,"Username Already Exists!")
+            messages.error(request, "Username Already Exists!")
             return redirect(Usersignuppage)
         elif signupdb.objects.filter(Signup_email=signup_email).exists():
-            messages.error(request,"Email Already Exists!")
+            messages.error(request, "Email Already Exists!")
             return redirect(Usersignuppage)
         elif signupdb.objects.filter(Signup_mobile=signup_mobile).exists():
-            messages.error(request,"Mobile Number Already Exists!")
+            messages.error(request, "Mobile Number Already Exists!")
             return redirect(Usersignuppage)
         else:
             ob.save()
-            messages.success(request,"Sign up Successfull!")
+            messages.success(request, "Sign up Successfull!")
             return redirect(Usersigninpage)
 
 
@@ -193,11 +219,11 @@ def login(request):
 
         if signupdb.objects.filter(Signup_username=signin_username, Signup_password=signin_password).exists():
             request.session['username'] = signin_username
-            messages.success(request,"Login Successfull!")
+            messages.success(request, "Login Successfull!")
             return redirect(Homepage)
 
         else:
-            messages.error(request,"Username or Password  Incorrect!")
+            messages.error(request, "Username or Password  Incorrect!")
             return redirect(Usersigninpage)
 
     else:
