ad019402cc9c895446fc8fb1f4a09461bd654be9
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2026-01-19 19:13:19 +0530

    set for live hosting installed gunicorn and whitenoise created static files and requiremet.txt added required root and midleware and host in settings

--- BookFilx/settings.py
+++ BookFilx/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1cywrdeq5yy7$7szw838(prsl5v$xt6^nx2(a6^w%89gonfdq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 MEDIA_URL='/media/'
